32f63a95462a7cc30e79c5850ce6a335426968dd
matej <matej.koscelnik@gmail.com>
2022-02-10 17:01:47 +0100

    vietor

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -20,10 +20,15 @@ public class Controller implements Initializable{
     Slider unava;
     @FXML
     AnchorPane panel;
+    @FXML
+    Slider sila_vetra;
+    @FXML
+    Slider smer_vetra;
 
     int perioda=0;
     double xmysi,ymysi,xmysiFix,ymysiFix;
     Random rn=new Random();
+    int cas_do_zatrasenia;
 
     public int galtonBoard(int pozicia, int hladina){
         if (hladina==0) return pozicia;
@@ -31,7 +36,10 @@ public class Controller implements Initializable{
         return galtonBoard(pozicia,hladina-1);
     }
 
-    public static int galton(int hladina , int pozicia){
+    public int short_galton(int najpravdepodobnejsi_vysledok,int zaciatok){
+        return galton(najpravdepodobnejsi_vysledok*2,zaciatok);
+    }
+    public int galton(int hladina , int pozicia){
         Random rd = new Random();
         if (hladina==0){
             return pozicia;
@@ -55,28 +63,41 @@ public class Controller implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        cas_do_zatrasenia = short_galton(15,0);
         new AnimationTimer() {
             @Override
             public void handle(long l) {
-                perioda++;
-                if (perioda % 15 == 0){
+
+                if (cas_do_zatrasenia == 0){
     /*              int rozptyl = (int) (10 * (unava.getValue()+1));
                     xmysi +=  rn.nextInt(2*rozptyl)-rozptyl;
                     ymysi +=  rn.nextInt(2*rozptyl)-rozptyl;
     */
-                    xmysiFix = xmysi + galton(10,-5) * ((unava.getValue()*3)+1);
+                    xmysiFix = xmysi + short_galton(5,-5) * ((unava.getValue()*3)+1);
 
-                    ymysiFix = ymysi + galton(10,-5) * ((unava.getValue()*3)+1);
+                    ymysiFix = ymysi + short_galton(5,-5) * ((unava.getValue()*3)+1);
 
+                    cas_do_zatrasenia = short_galton(15,0);
                 }
                 kriz.setX(kriz.getX()*0.97+xmysiFix*0.03);
                 kriz.setY(kriz.getY()*0.97+ymysiFix*0.03);
+
+                cas_do_zatrasenia--;
             }
         }.start();
     }
 
     public void pifpaf(MouseEvent mouseEvent) {
-        Circle c = new Circle(kriz.getX()+kriz.getFitWidth()/2,kriz.getY()+kriz.getFitHeight()/2,10);
+        Circle c = new Circle(kriz.getX()+kriz.getFitWidth()/2,kriz.getY()+kriz.getFitHeight()/2,5);
+        c=vietor(c);
         panel.getChildren().add(c);
+    }
+    public Circle vietor(Circle c){
+        Circle vystup =c;
+
+        vystup.setCenterY(vystup.getCenterY()+(Math.sin(Math.toRadians(smer_vetra.getValue()-45+short_galton(45,0))))*short_galton((int) sila_vetra.getValue(),0));
+        vystup.setCenterX(vystup.getCenterX()+(Math.cos(Math.toRadians(smer_vetra.getValue()-45+short_galton(45,0))))*short_galton((int) sila_vetra.getValue(),0));
+
+        return vystup;
     }
 }
